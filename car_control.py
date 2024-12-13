2699bf743cfda0f5c1ccc3972e790f7fec573b3a
jiye <jiye3180@naver.com>
2024-12-13 15:35:15 +0900

    전진, 후진 추가가

--- car_control.py
+++ car_control.py
@@ -1,13 +1,5 @@
 class CarControl:
-    """Controls the movement of the car by managing motor speeds and directions."""
-
     def __init__(self, car):
-        """
-        Initializes the CarControl instance with a car object.
-
-        Args:
-            car: An instance of the car class that provides methods to control the car's movement.
-        """
         self.car                   = car
         self.motor_up_speed        = 68
         self.motor_corner_speed    = 58
@@ -16,13 +8,6 @@ class CarControl:
         self.right_increased_speed = 85
 
     def control(self, direction):
-        """
-        Controls the car's movement based on the specified direction.
-
-        Args:
-            direction (str): The direction command to control the car.
-                             Possible values: "REDUCED_LEFT", "REDUCED_RIGHT", "UP", "LEFT", "RIGHT".
-        """
         if direction == "REDUCED_LEFT":
             self.car.Car_Run(self.left_reduced_speed, self.right_increased_speed)
         elif direction == "REDUCED_RIGHT":
@@ -33,9 +18,10 @@ class CarControl:
             self.car.Car_Left(self.motor_down_speed, self.motor_corner_speed)
         elif direction == "RIGHT":
             self.car.Car_Right(self.motor_corner_speed, self.motor_down_speed)
+        elif direction == "BACK":
+            self.car.Car_Back(self.motor_down_speed, self.motor_down_speed)
+        elif direction == "RUN":
+            self.car.Car_Run(self.motor_up_speed, self.motor_up_speed)
 
     def stop(self):
-        """
-        Stops the car by calling the Car_Stop method of the car object.
-        """
         self.car.Car_Stop()
